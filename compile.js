04201fd7c9a01674a95160b209047324e5ae8c78
Khuzama Shahid <skhuzamashahid@gmail.com>
2020-10-16 00:18:05 +0500

    fix a bug

--- compile.js
+++ compile.js
@@ -1,8 +1,10 @@
 const path = require('path');
-const fs = path('fs');
+const fs = require('fs');
 const solc = require('solc');
 
 const InboxPath = path.resolve(__dirname,"contracts","Inbox.sol");
+console.log(InboxPath)
 const source = fs.readFileSync(InboxPath,"utf8");
-
-module.exports = solc.compile(source,1).contracts[":Inbox"];
+console.log(source)
+// console.log(solc.compile(source))
+module.exports = solc.compile(source).contracts[":Inbox"];
--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.4.16 <0.8.0;
 
 contract Inbox {
